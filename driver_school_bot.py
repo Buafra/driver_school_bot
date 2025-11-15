9588156d246627a41180d1677161523dc9cda55c
North <108111087+Buafra@users.noreply.github.com>
2025-11-15 10:30:32 +0400

    Delete driver_school_bot.py

--- driver_school_bot.py
+++
@@ -1,3 +0,0 @@
-# driver_school_bot.py
-# Due to message length limits, please paste your full bot code here manually.
-# This file is ready for replacement with the final content you already tested.

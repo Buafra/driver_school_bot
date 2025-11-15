538ed6a9010b751fbb88540ab20d6e84df8416ab
North <108111087+Buafra@users.noreply.github.com>
2025-11-15 10:22:51 +0400

    Add files via upload

---
+++ driver_school_bot.py
@@ -0,0 +1,3 @@
+# driver_school_bot.py
+# Due to message length limits, please paste your full bot code here manually.
+# This file is ready for replacement with the final content you already tested.
